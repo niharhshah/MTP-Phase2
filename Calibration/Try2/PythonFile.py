d36a19f1b2ca8c631b3e9df8a781613d683cb4a6
Nihar <nihars@iitb.ac.in>
2022-05-26 15:04:23 +0530

    New Shell Script is stored in Calibration/Try2/ The data is working and moving on to data collection FFS

--- Calibration/Try2/PythonFile.py
+++ Calibration/Try2/PythonFile.py
@@ -1,35 +1,87 @@
-from roboclaw_3 import Roboclaw
-from datetime import datetime
-from time import sleep 
-address = 0x80
-debug = 0
-rc = Roboclaw("/dev/ttyACM0",115200)
-rc.Open()
-now = datetime.now()
-
+import serial
+from time import sleep
+# import sys
+# odoRead = open("demoFile.txt", "r")
 odoRead = open("CalibrationFiles/OdemData.txt", "r")
-if debug:
-    print("file open success!")
+# if debug:
+#     print("file open success!")
 lines = odoRead.readlines()
 length = len(lines) -1
 inter = lines[length].split()
-if debug:
-    print(int(inter[1]))
-    print(int(inter[2]))
+# if debug:
+#     print(int(inter[1]))
+#     print(int(inter[2]))
+M2_enc = int(inter[0])
 M1_enc = int(inter[1])
-M2_enc = int(inter[2])
-rc.SetEncM1(address,M1_enc)
-rc.SetEncM2(address,M2_enc)
+pic_no = int(inter[3])
+print(M2_enc,M1_enc,pic_no)
 odoRead.close()
+# exit()
+# try:
 odoWrite = open("CalibrationFiles/OdemData.txt", "a")
-for i in range(50):
-    rc.SpeedM1M2(address,3000,4000)
-    M1_enc = rc.ReadEncM1(address)
-    M2_enc = rc.ReadEncM2(address)
-    sleep(0.1)
-    if debug:
-        print(M1_enc, M2_enc)
-    strng = now.strftime("%H%M%S%f") +" "+str(M1_enc[1]) + " " + str(M2_enc[1]) + " 0\n" 
-    odoWrite.write(strng)
-rc.SpeedM1M2(address,0,0)
-odoWrite.close()
+# odoWrite = open("demoFile.txt", "a")
+usb = serial.Serial("/dev/ttyUSB0",9600)
+# usb.write(b"1")
+usb.close()
+print("Port closed")
+usb.open()
+print("Port Opened")
+sleep(0.5) #  Wait for the serial communication to begin
+# except:
+#     print("Please Connect USB device default address is /dev/ttyUSB0")
+#     exit()
+encoders = ""
+str_enc = ""
+flag = 0
+while True:
+    try:
+        str_enc1 = ""
+        str_enc2 = ""
+        flag = 0
+        M1_encr = 0;
+        M2_encr = 0;
+        usb.write(b"12")
+        sleep(0.1)
+        usb.write(b"E")
+        encoders = usb.read_until()
+        # print(encoders)
+        for i in encoders:
+            if((i>= 48 and i < 58)or i == 32):
+                if(i == 32):
+                    flag = 1
+                else:
+                    if flag:
+                        str_enc1 += str(i-48)
+                    else:
+                        str_enc2 += str(i-48)
+        M1_encr = int(str_enc1) + M1_enc
+        M2_encr = int(str_enc2) + M2_enc
+        print(str(M2_encr),"\t",int(M1_encr))
+        odoWrite.write(str(M2_encr) + " "+ str(M1_encr) + " 0 " + str(pic_no)+"\n")
+    # except PortNotOpenError:
+    #     print("Port Not open Error!")
+    #     exit()
+    except KeyboardInterrupt:
+        print("Exiting....")
+        usb.write(b"E")
+        encoders = usb.read_until()
+        # print(encoders)
+        for i in encoders:
+            if((i>= 48 and i < 58)or i == 32):
+                if(i == 32):
+                    flag = 1
+                else:
+                    if flag:
+                        str_enc1 += str(i-48)
+                    else:
+                        str_enc2 += str(i-48)
+        M1_encr = int(str_enc1) + M1_enc
+        M2_encr = int(str_enc2) + M2_enc
+        print("Saving The file...")
+        odoWrite.write(str(M2_encr) + " "+ str(M1_encr) + " 1 " + str(pic_no+1)+"\n")
+        usb.write(b"8")
+        odoWrite.close()
+        usb.close()
+        break
+# odoWrite.write(str(M2_encr) + " "+ str(M1_encr) + " 1" + str(pic_no+1)+"\n")
+exit()
--- Calibration/Try2/setup.sh
+++ Calibration/Try2/setup.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 rm CalibrationFiles/Depth/*
 rm CalibrationFiles/Color/*
-echo "Timestamp 0 0 0" > CalibrationFiles/OdemData.txt
-for i in {0..9}
+echo "0 0 0 0" > CalibrationFiles/OdemData.txt
+for i in {0..12}
 do
-	sudo python3 PythonFile.py
-	python3 Intermediate.py
 	rs-save-to-disk
-	mv rs-save-to-disk-output-Depth.png rs-save-to-disk-output-Depth-${i}.png
-	mv rs-save-to-disk-output-Color.png rs-save-to-disk-output-Color-${i}.png
+	python3 PythonFile.py
+	mv rs-save-to-disk-output-Depth.png Depth-${i}.png
+	mv rs-save-to-disk-output-Color.png Color-${i}.png
 done
 rm *.csv
-mv rs-save-to-disk-output-Depth-* CalibrationFiles/Depth/
-mv rs-save-to-disk-output-Color-* CalibrationFiles/Color/
+mv Depth-* CalibrationFiles/Depth/
+mv Color-* CalibrationFiles/Color/
 
